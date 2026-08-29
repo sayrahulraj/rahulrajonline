40fd1c5a5f8ae337d1a4faa523c2ebd7fe377663
Rahul Raj <rajdeveloper017@gmail.com>
2026-08-30 00:02:36 +0530

    adding .js

--- api/auth/login.ts
+++ api/auth/login.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { sql } from '../_lib/db.js.js';
+import { sql } from '../_lib/db.js.js.js';
 import { signAdminToken } from '../_lib/auth.js.js';
 import { handlePreflight, methodNotAllowed, serverError } from '../_lib/http.js.js';
 
